955769bd5941b7aacae781394775f7e5201ba063
proxy-m <wproxym@gmail.com>
2022-12-06 22:42:52 +0300

    English example

--- samples/client4yandex.php
+++ samples/client4yandex.php
@@ -3,28 +3,28 @@ require_once('./lib/nusoap.php');
 
 $wsdlurl = 'https://api-sandbox.direct.yandex.com/v5/campaigns?wsdl'; /// 'https://api-sandbox.direct.yandex.ru/live/v4/wsdl/';
 $token = 'YOUR TOKEN';
-$locale = 'ru';
+$locale = 'en';
 
-// Инициализация NuSOAP-клиента
+// Initialization of NuSOAP-client
 $client = new nusoap_client($wsdlurl, 'wsdl');
 
-# Параметры NuSOAP-клиента
+# Parameters of NuSOAP-client
 $client->authtype = 'bearer'; /// 'basic';
 $client->decode_utf8 = 0;
 $client->soap_defencoding = 'UTF-8';
 
-// Формирование заголовков SOAP-запроса
+// Adding headers for SOAP-request
 $client->setCredentials('token', $token, 'bearer'); ///$client->setHeaders("<token>$token</token>\n     <locale>$locale</locale>");
 
-// Выполнение запроса к серверу API Директа
+// Call request to server (API Yandex Direct)
 $result = $client->call(/*'GetClientInfo'*/'get', array('SelectionCriteria' => (object) array(), 'FieldNames' => array('Id', 'Name')));
 
-// Вывод запроса и ответа
-echo "Запрос:<pre>".htmlspecialchars($client->request, ENT_QUOTES)."</pre>";
-echo "Ответ:<pre>".htmlspecialchars($client->response, ENT_QUOTES)."</pre>";
+// Output of request and response
+echo "Request (to server):<pre>".htmlspecialchars($client->request, ENT_QUOTES)."</pre>";
+echo "Response (from server):<pre>".htmlspecialchars($client->response, ENT_QUOTES)."</pre>";
 
 
-// Вывод отладочной информации 
+// Output of debug info
 echo '<hr><pre>'.htmlspecialchars($client->debug_str, ENT_QUOTES).'</pre>';
 
 ?>
